9bc719d7b5e322fd0692fc11f9bc1cc7150a5b94
Supakorn Charamat <ohmsupakorn@gmail.com>
2024-05-26 21:49:11 +0700

    add env

--- db.js
+++ db.js
@@ -1,22 +1,10 @@
-// db.js
 const sql = require("mssql");
 
-// const config = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB_NAME,
-//   options: {
-//     encrypt: true, // Use this if you're on Windows Azure
-//     trustServerCertificate: true, // Change to true for local dev / self-signed certs
-//   },
-// };
-
 const config = {
-  user: "ptt-admin",
-  password: "PT12345!",
-  server: "ptt-test-server.database.windows.net", // You can use 'localhost\\instance' to connect to named instance
-  database: "pttdatabase",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   options: {
     encrypt: true, // Use this if you're on Windows Azure
     trustServerCertificate: true, // Change to true for local dev / self-signed certs
